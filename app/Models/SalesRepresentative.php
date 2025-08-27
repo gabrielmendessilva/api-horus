aec06d279ec79f6bc1301f918cd9e7be8ae1be01
Gabriel <mendes.gabriel@icloud.com>
2025-08-27 11:22:31 -0300

    representante

---
+++ app/Models/SalesRepresentative.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SalesRepresentative extends Model
+{
+    protected $connection = 'pgsql';
+    protected $table = "sales_representative";
+}
